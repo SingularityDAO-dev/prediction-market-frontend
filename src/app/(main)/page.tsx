8252e3e36c72ec99c28ec3908d4cbf6240f5dbe3
SingularityDAO-dev <79306596+SingularityDAO-dev@users.noreply.github.com>
2026-02-22 01:27:34 +0100

    Add "use client" directive to MainLayout and page components for client-side rendering

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import MainLayout from "../MainLayout";
 import HomePage from "../page";
 
--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Sidebar } from "@/components/layout/Sidebar";
 import { TopBar } from "@/components/layout/TopBar";
 import { Ticker } from "@/components/layout/Ticker";
